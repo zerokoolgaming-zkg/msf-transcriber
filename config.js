9dec2dae2f491bb524431277da001161581b0160
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-09 13:58:43 -0600

    Update GOOGLE_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Optional config. You can also set URL via the Settings box. */
-// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxIujmvJvWFvx3eOg4E73wPKoIOQze7smKsEoQE9PC_KUqbdALncUt6lyUpnf_d6wW9/exec';
+// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw3-4CR7QsOAJnxYQh0UeGED5Q9p6EseRJWkpgposOPLCMrW-DoJPfbqN8bZoN-BQO5/exec';
