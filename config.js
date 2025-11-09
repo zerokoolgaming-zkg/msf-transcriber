cac89ffd4dc5564128c47d1f0c2f60e3561a4a42
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-08 23:01:51 -0600

    Change Google Script URL in config.js
    Updated the Google Apps Script Web App URL in config.js.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxhGEIgTm6r-BA3XIoSqOSxIMyqwal4F7r4xZvX8U_1kqrVNIc5VFPKwkdrq6yJVSkL/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzDStbZZzqEW2dZKI3Um-AzsP7HgvOwm_1Cdi-A_73zwGkyQUx2gsK0DfZMrnh8VKy/exec";
