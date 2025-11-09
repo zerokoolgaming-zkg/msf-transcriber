1c182a500420f9cb3e07cd58e0c945fb5220f8e8
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-08 23:43:59 -0600

    Update Google Apps Script Web App URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyqqGr4IAl8MJW12u2Y83lOl96lVUlw1hd4-F8OlfDD-gadlp5uy5DNjzml2i-f-F1p/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxIujmvJvWFvx3eOg4E73wPKoIOQze7smKsEoQE9PC_KUqbdALncUt6lyUpnf_d6wW9/exec";
