eea25a2a9a43d1a6a13fbff5e0b9a8b7b27fcbee
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-09 15:18:58 -0600

    Comment out GOOGLE_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Optional config. You can also set URL via the Settings box. */
-// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz9LskpHXDPQGFn5Ti8yQIxbieLJvQzCj7dEWmw7XEakwzVELUFCgV-p7cwEdyUHJeu/exec';
+// const GOOGLE_SCRIPT_URL = '1OwPJT-MFL2uDoy6AQgh3cY_V3-suDHn3o3Ir-WLdHp4';
