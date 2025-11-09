781aa34b11ff1d80f992b27837c0a73aa4e1f364
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-09 15:40:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Optional config. You can also set URL via the Settings box. */
-// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbydHlMQxqDIZojxz-TNMgVuT_hmwPpXD_BaZL570MGodWocPaUfX28LjjwqtAiAtH3F/exec';
+// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwyI_ENEAyotl3hj5dY22F92rxhYfAZO_g7cVVANwUFxh8musVgGnnVH7BXZRRQJjWp/exec';
