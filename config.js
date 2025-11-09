1e3265619118c7d0732db10ab9f32064cb2527b7
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-08 23:35:31 -0600

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// Replace with your deployed Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwtfexGxqVhIleFBy72xePvW7kgfqYG5h9779sky6DyxVmAzUp1iYnEd0LP_NCIlDR4/exec";
