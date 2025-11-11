23487f433071b14c2e2f0cdfb1a8bf7bea11d247
zerokoolgaming-zkg <zerokoolgaming@gmail.com>
2025-11-11 00:14:46 -0600

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-/* Optional config. You can also set URL via the Settings box. */
-// const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwlgrZw3EHkuSEmduC66Of1h15u4RLTjiQ6fZlPTxYaVDLCG8Lzwa6rhC8HtjhfHPY/exec';
